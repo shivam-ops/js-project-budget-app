681a3a07bd299a1f3430fc7e0d04a9870b6bb5c7
Shivam Joshi <shivamjoshi0822@gmail.com>
2020-06-16 08:27:27 +0530

    calculating the budget

--- app.js
+++ app.js
@@ -13,6 +13,16 @@ var budgetController = (function() {
     };
 
 
+    var calculateTotal = function(type) {
+        var sum = 0;
+        data.allItems[type].forEach(function(cur) {
+            sum += cur.value;
+        });
+
+        data.totals[type] = sum;
+    };
+
+
     var data = {
         allItems: {
             exp: [],
@@ -21,7 +31,9 @@ var budgetController = (function() {
         totals: {
             exp: 0,
             inc: 0
-        }
+        },
+        budget: 0,
+        percentage: -1,
     };
 
     return {
@@ -51,6 +63,32 @@ var budgetController = (function() {
             //Return the new element
             return newItem;
             
+        },
+
+        calculateBudget: function() {
+            // calculate total income and expenses
+            calculateTotal('exp');
+            calculateTotal('inc');
+
+            // calculate the budget: income - expenses
+            data.budget = data.totals.inc - data.totals.exp;
+
+            // calculate the percentage of income that we spent
+            if(data.totals.inc > 0) {
+                data.percentage = Math.round((data.totals.exp / data.totals.inc) * 100);
+            } else {
+                data.percentage = -1;
+            }
+            
+        },
+
+        getBudget: function() {
+            return {
+                budget: data.budget,
+                totalInc: data.totals.inc,
+                totalExp: data.totals.exp,
+                percentage: data.percentage
+            }
         },
 
         testing: function() {
@@ -140,10 +178,11 @@ var controller = (function(budgetCtrl, UICtrl) {
     var updateBudget = function() {
 
         //1. Calculate the budge
-
+        budgetCtrl.calculateBudget();
         //2. Return the budget
-
+        var budget = budgetCtrl.getBudget();
         //3. Display the budget on the UI
+        console.log(budget);
     }
 
 
@@ -165,7 +204,6 @@ var controller = (function(budgetCtrl, UICtrl) {
             //5. Calculate and update budget
             updateBudget();
         }
-
     }
     
 
